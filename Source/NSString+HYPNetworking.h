f8825804dc645c789d3351e800044d953b53af38
Elvis Nuñez <elvisnunez@me.com>
2014-12-17 17:33:02 +0100

    Add .gitignore

---
+++ Source/NSString+HYPNetworking.h
@@ -0,0 +1,13 @@
+//
+//  NSString+HYPNetworking.h
+//  NSManagedObject-HYPPropertyMapper
+//
+//  Created by Elvis Nuñez on 12/17/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (HYPNetworking)
+
+@end
---
+++ Source/NSString+HYPNetworking.m
@@ -0,0 +1,13 @@
+//
+//  NSString+HYPNetworking.m
+//  NSManagedObject-HYPPropertyMapper
+//
+//  Created by Elvis Nuñez on 12/17/14.
+//
+//
+
+#import "NSString+HYPNetworking.h"
+
+@implementation NSString (HYPNetworking)
+
+@end
